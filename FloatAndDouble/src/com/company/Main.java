a305a893312b3c3a26c308072c7063a4305087b5
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-15 18:37:08 -0700

    F and D

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value =" + myMinFloatValue);
+        System.out.println("Float Maximum Value =" + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value =" + myMinDoubleValue);
+        System.out.println("Double Maximum Value =" + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.25 / 3.00;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatVale = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+    }
+}
